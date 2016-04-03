5205120ec59ab85cb659c7eac17def30da78fe36
suddha_satwa <suddha90@gmail.com>
2016-04-04 02:46:23 +0530

    update

--- server.js
+++ server.js
@@ -38,10 +38,9 @@ function mailSender(toEmail){
 }
 
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
+res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 }
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
