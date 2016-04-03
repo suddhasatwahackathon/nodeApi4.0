138a60c175b49daa25eca5013822cc8d496a02b4
suddha_satwa <suddha90@gmail.com>
2016-04-04 02:19:21 +0530

    update

--- src/controller/subscription/index.js
+++ src/controller/subscription/index.js
@@ -28,7 +28,7 @@ exports.add = function(req, res) {
     if (err) return console.log(err); 
     return res.send(res);
   });*/
-console.log('hello send me rquest',req.body);
+console.log('hello send me rquest',req);
 }
 
 exports.delete = function(req, res){
