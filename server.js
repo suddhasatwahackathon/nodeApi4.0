43f664fe6a748ad2bad21deb3a10f47f8dc1d651
suddha_satwa <suddha90@gmail.com>
2016-04-04 02:41:47 +0530

    update

--- server.js
+++ server.js
@@ -38,10 +38,10 @@ function mailSender(toEmail){
 }
 
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    //next();
+      res.setHeader('Access-Control-Allow-Origin', 'http://localhost:9000');
+/*     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');*/
+    next();
 }
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
