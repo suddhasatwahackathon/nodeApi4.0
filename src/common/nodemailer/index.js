b1baadd198eddf3de2a6a7295cdf3c34b03fa891
Suddhasatwa Gupta <suddhasatwa.g@plancess.com>
2016-04-04 15:40:38 +0530

    update the mailer object

--- src/common/nodemailer/index.js
+++ src/common/nodemailer/index.js
@@ -1,25 +1,29 @@
-module.exports = function(nodemailer){
-	var transporter = nodemailer.createTransport({
-	  service : "gmail",
-	  auth    : {
-	    user     : "suddha90@gmail.com",
+ var nodemailer = require('nodemailer');
 
-	    // neither of these work
-	    pass : "satwa900900",
-	    // pass : 'my"annoying\'password',
-	  }
-	});
-	return transporter;
-/*	var mailSender=function(toEmail){
-	// setup e-mail data with unicode symbols
-		var mailOptions = {
-		    from: '"Slipbeep" <support@slipbeep.com>', // sender address
-		    to: toEmail, // list of receivers
-		    subject: "Thanks for subscribing", // Subject line
-		    text: "Thanks for subscribing", // plaintext body
-		    html: "<p>All Gaadi solutions there when we will be finally here.</p><p>Subscribe early for , Free services, Mega cashbacks & discounts After subscription of the subscriber.Thanks for ur interest, we will notify u.</p><pThanks Slipbeep</p>" // html body
-			}
-		return mailOptions;
-	}*/
+ 	    var transporter = nodemailer.createTransport({
+      service : "gmail",
+      auth    : {
+        user     : "slipbeepnodemailer@gmail.com",
 
-}
+        // neither of these work
+        pass : "nodemailer@12345",
+        // pass : 'my"annoying\'password',
+      }
+    });
+    function mailParam(email){
+    	var mailOptions = {
+        from: '"Slipbeep" <support@slipbeep.com>', // sender address
+        to: email, // list of receivers
+        subject: "Thanks for subscribing", // Subject line
+        text: "Thanks for subscribing", // plaintext body
+        html: "<p>All Gaadi solutions there when we will be finally here.</p><p>Subscribe early for , Free services, Mega cashbacks & discounts After subscription of the subscriber.Thanks for ur interest, we will notify u.</p><pThanks Slipbeep</p>" // html body
+      } 
+      return mailOptions;
+    }    
+    
+
+    var exportObj = {
+    	transporterObj:transporter
+    	//mailOptionsObj:mailOptions
+    };
+    module.exports = exportObj; 
--- src/controller/subscription/index.js
+++ src/controller/subscription/index.js
@@ -1,29 +1,6 @@
 var subscriptions = require('./../../model/subscription');
-var nodemailer = require('nodemailer');
-//require('./../../common/nodemailer');
-// create reusable transporter object using the default SMTP transport
-/*var transporter = nodemailer.createTransport({
-  service : "gmail",
-  auth    : {
-    user     : "suddha90@gmail.com",
-
-    // neither of these work
-    pass : "satwa900900",
-    // pass : 'my"annoying\'password',
-  }
-});*/
-/*function mailSender(toEmail){
-// setup e-mail data with unicode symbols
-  var mailOptions = {
-      from: '"Slipbeep" <support@slipbeep.com>', // sender address
-      to: toEmail, // list of receivers
-      subject: "Thanks for subscribing", // Subject line
-      text: "Thanks for subscribing", // plaintext body
-      html: "<p>All Gaadi solutions there when we will be finally here.</p><p>Subscribe early for , Free services, Mega cashbacks & discounts After subscription of the subscriber.Thanks for ur interest, we will notify u.</p><pThanks Slipbeep</p>" // html body
-    }
-  return mailOptions;
-}
-*/
+var mailer = require('./../../common/nodemailer');
+console.log("mailer obj is",mailer)
 exports.findAll = function(req, res){
   subscriptions.find({},function(err, results) {
     return res.send(results);
@@ -51,28 +28,12 @@ exports.update = function(req, res) {
 exports.add = function(req, res, next) {
   subscriptions.create(req.body, function (err, subscriptions) {
     if (err) return console.log(err); 
-    //mailOptions = mailSender(req.body.email);   
-    var transporter = nodemailer.createTransport({
-      service : "gmail",
-      auth    : {
-        user     : "slipbeepnodemailer@gmail.com",
 
-        // neither of these work
-        pass : "nodemailer@12345",
-        // pass : 'my"annoying\'password',
-      }
-    });    
-    var mailOptions = {
-        from: '"Slipbeep" <support@slipbeep.com>', // sender address
-        to: req.body.email, // list of receivers
-        subject: "Thanks for subscribing", // Subject line
-        text: "Thanks for subscribing", // plaintext body
-        html: "<p>All Gaadi solutions there when we will be finally here.</p><p>Subscribe early for , Free services, Mega cashbacks & discounts After subscription of the subscriber.Thanks for ur interest, we will notify u.</p><pThanks Slipbeep</p>" // html body
-      }         
+/*    mailOptions = mailSender(req.body.email);   
     transporter.sendMail(mailOptions, function(error, info){
         if(error){
             return console.log(error);
-        }
+        }*/
         console.log('Message info: ' + info);
         return res.send(info);        
     });    
