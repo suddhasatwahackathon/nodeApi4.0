0820a1a5b97efb003808bdef26b88f720fd99d71
suddha_satwa <suddha90@gmail.com>
2016-04-04 10:41:39 +0530

    update cors

--- src/controller/subscription/index.js
+++ src/controller/subscription/index.js
@@ -29,7 +29,7 @@ exports.add = function(req, res, next) {
     if (err) return console.log(err); 
     var mailOptions = {
         from: '"Slipbeep" <support@slipbeep.com>', // sender address
-        to: toEmail, // list of receivers
+        to: req.body.email, // list of receivers
         subject: "Thanks for subscribing", // Subject line
         text: "Thanks for subscribing", // plaintext body
         html: "<p>All Gaadi solutions there when we will be finally here.</p><p>Subscribe early for , Free services, Mega cashbacks & discounts After subscription of the subscriber.Thanks for ur interest, we will notify u.</p><pThanks Slipbeep</p>" // html body
