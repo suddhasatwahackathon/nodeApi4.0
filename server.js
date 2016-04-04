4eb1e72f5ff19ccf7d8cde01c9eb4c10dc7a7b3b
suddha_satwa <suddha90@gmail.com>
2016-04-04 10:19:36 +0530

    update cors

--- server.js
+++ server.js
@@ -42,6 +42,7 @@ res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 }
+app.use(allowCrossDomain);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 require('./src/model/subscription');
