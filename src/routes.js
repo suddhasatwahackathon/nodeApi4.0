15c29cc75f70a07ff70d508e2627936d9945605c
suddha_satwa <suddha90@gmail.com>
2016-04-07 02:18:21 +0530

    update location

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,13 @@ module.exports = function(app){
   app.post('/subscriptions', subscriptions.add);
   app.delete('/subscriptions/:id', subscriptions.delete);
   app.get('/import', subscriptions.import);  
-
+  var searchparking = require('./controller/searchparking');
+  app.get('/searchparking', searchparking.findAll); 
+  app.get('/searchparking/:id', searchparking.findById); 
+  app.put('/searchparking/:id', searchparking.update);
+  app.post('/searchparking', searchparking.add);
+  app.delete('/searchparking/:id', searchparking.delete);
+  app.get('/import', searchparking.import);  
   app.get('/hello', function(req, res) {
       res.send('Hello New York\n');
   });
