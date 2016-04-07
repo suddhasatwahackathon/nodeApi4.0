dc3afe0444005f941c9b1c4aa3508d9fcdc76bbe
suddha_satwa <suddha90@gmail.com>
2016-04-07 09:16:30 +0530

    uodate the api

--- src/model/searchparking/index.js
+++ src/model/searchparking/index.js
@@ -7,14 +7,14 @@ var searchParkingschema = new Schema({
   country: String,
   zip: Number
 });
-subscriptions = mongoose.model('Searchparking', searchParkingschema,'searchParking');
+searchparking = mongoose.model('Searchparking', searchParkingschema,'searchParking');
 exports.findAllObj = function(res){
-  subscriptions.find({},function(err, results) {    
+  searchparking.find({},function(err, results) {    
     return res.send(results);
   });
 }
 exports.addObj = function(req,res) {
-  subscriptions.create(req.body, function (err, results) {
+  searchparking.create(req.body, function (err, results) {
     if (err) return console.log(err); 
     res.send(results);    
   });
